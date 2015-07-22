641d4c9c1e681de0aaec78dd7038abffb0498de5
Constance Jiang <conzjiang@gmail.com>
2015-07-22 16:26:46 -0400

    target='_blank' to contact links.

--- js/build/about.js
+++ js/build/about.js
@@ -8,11 +8,18 @@
         React.createElement("article", {className: "about"}, 
           React.createElement("ul", {className: "contact-links"}, 
             React.createElement("li", null, "Based in New York, NY"), 
-            React.createElement("li", null, React.createElement("a", {href: "#"}, "Download Resume (PDF)")), 
+
+            React.createElement("li", null, React.createElement("a", {href: "#", target: "_blank"}, "Download Resume (PDF)")), 
+
             React.createElement("li", null, 
-              React.createElement("a", {href: "https://www.linkedin.com/in/conzjiang"}, "LinkedIn")
+              React.createElement("a", {href: "https://www.linkedin.com/in/conzjiang", 
+                target: "_blank"}, "LinkedIn")
             ), 
-            React.createElement("li", null, React.createElement("a", {href: "https://github.com/conzjiang"}, "Github")), 
+
+            React.createElement("li", null, 
+              React.createElement("a", {href: "https://github.com/conzjiang", target: "_blank"}, "Github")
+            ), 
+
             React.createElement("li", null, React.createElement("a", {href: "mailto:conzjiang@gmail.com"}, "Email"))
           )
         )
--- js/src/about.js
+++ js/src/about.js
@@ -8,11 +8,18 @@
         <article className="about">
           <ul className="contact-links">
             <li>Based in New York, NY</li>
-            <li><a href="#">Download Resume (PDF)</a></li>
+
+            <li><a href="#" target="_blank">Download Resume (PDF)</a></li>
+
             <li>
-              <a href="https://www.linkedin.com/in/conzjiang">LinkedIn</a>
+              <a href="https://www.linkedin.com/in/conzjiang"
+                target="_blank">LinkedIn</a>
             </li>
-            <li><a href="https://github.com/conzjiang">Github</a></li>
+
+            <li>
+              <a href="https://github.com/conzjiang" target="_blank">Github</a>
+            </li>
+
             <li><a href="mailto:conzjiang@gmail.com">Email</a></li>
           </ul>
         </article>
